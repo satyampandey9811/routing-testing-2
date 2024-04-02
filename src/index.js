4fceec21b682e263c1d367bfc0391f76999b73b4
Satyam Pandey <satyampandey9811@gmail.com>
2024-04-03 01:49:51 +0530

    testing final

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Router>
     <App />
-  </Router>
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
