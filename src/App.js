b76e7742a8ca96bc96b62199a276f642ebce746f
Satyam Pandey <satyampandey9811@gmail.com>
2024-04-03 00:38:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/signup' element={<Signup></Signup>}></Route>
+        </Routes>
+
+        <header className='App-header'>
+          <p>
+            <a href='login'>Go to login page </a>
+          </p>
+          <p>
+            <a href='/signup'>Go to signup page</a>
+          </p>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
